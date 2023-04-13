93d9c1658a2c141c53779a436dd036ec85b25a7d
ikhdaamel <ikhda.new45@gmail.com>
2023-04-13 14:36:25 +0700

    menampilkan elemen dalam array

--- LinearSearch_131/LinearSearch_131.cpp
+++ LinearSearch_131/LinearSearch_131.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int arr[20];			//arary to be searched 
 int n;					//number of elements in the array
 int i;					//index of array element
+
+void input()
 {
-   
+	while (true)
+	{
+		cout << "enter the number of elements in the array: ";
+   }
 }
